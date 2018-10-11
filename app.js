3f90f4dc61f4365b1515dd19ff9ea9b060a0fb94
ivan <ivan.pap18@gmail.com>
2018-10-11 10:34:45 -0500

    primer comit

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    let salida = {
+        nombre: 'ivan',
+        edad: 32,
+        url: req.url
+    }
+    res.write(JSON.stringify(salida));
+    //res.write('hola mundo');
+    res.end();
+}).listen(8080);
+
+console.log('escuchando el puerto 80');
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helprs');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'))
+
+//express hbs engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'ivan',
+        anio: new Date().getFullYear()
+    });
+
+});
+app.get('/about', (req, res) => {
+    res.render('about', {
+        anio: new Date().getFullYear()
+    });
+
+});
+
+app.listen(3000, () => {
+    console.log('escuchando peticiones puerto 3000');
+
+});
